63d8ab08c5a0a9459f354eb9f4bb5b04f64dc84d
Aliana Canizares <ac4820@princetonedu>
2026-04-02 12:53:32 -0400

    for part 2

--- 2DT/checkerDT.c
+++ 2DT/checkerDT.c
@@ -47,14 +47,18 @@ boolean CheckerDT_Node_isValid(Node_T oNNode) {
    Returns FALSE if a broken invariant is found and
    returns TRUE otherwise.
 
+   During the travesal it counts the node that it is checking and
+   adds it to nodeCount.
+
    You may want to change this function's return type or
    parameter list to facilitate constructing your checks.
    If you do, you should update this function comment.
 */
-static boolean CheckerDT_treeCheck(Node_T oNNode) {
+static boolean CheckerDT_treeCheck(Node_T oNNode, size_t *nodeCount) {
    size_t ulIndex;
 
    if(oNNode!= NULL) {
+      (*nodeCount)++;
 
       /* Sample check on each node: node must be valid */
       /* If not, pass that failure back up immediately */
@@ -98,7 +102,7 @@ static boolean CheckerDT_treeCheck(Node_T oNNode) {
 
          /* if recurring down one subtree results in a failed check
             farther down, passes the failure back up immediately */
-         if(!CheckerDT_treeCheck(oNChild))
+         if(!CheckerDT_treeCheck(oNChild, nodeCount))
             return FALSE;
       }
    }
@@ -109,15 +113,31 @@ static boolean CheckerDT_treeCheck(Node_T oNNode) {
 boolean CheckerDT_isValid(boolean bIsInitialized, Node_T oNRoot,
                           size_t ulCount) {
 
+
+   size_t nodeCount = 0;
+   
    /* Sample check on a top-level data structure invariant:
       if the DT is not initialized, its count should be 0. */
-   if(!bIsInitialized)
+   if(!bIsInitialized && ulCount != 0)
       if(ulCount != 0) {
          fprintf(stderr, "Not initialized, but count is not 0\n");
          return FALSE;
       }
 
-   /* Now checks invariants recursively at each node from the root. */
-   return CheckerDT_treeCheck(oNRoot);
+   
+   /* Now checks invariants recursively at each node from the root, and counts
+   the actual amount of nodes in the tree */
+   if(!CheckerDT_treeCheck(oNRoot, &nodeCount))
+      return FALSE;
+
+   /* Checks the actual number of nodes against the expected number and returns
+      an error on mismatch. */
+   if (nodeCount != ulCount) {
+      fprintf(stderr, "Count Mismatch\n");
+         return FALSE;
+   }
+
+   /* Returns true if all previous recursive checks passed*/
+   return TRUE;
 }
 
